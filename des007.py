08fc3a6051a91d7793fc43014ee3b0c6e9436d61
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-02-25 14:44:07 -0300

    Create des007.py

---
+++ des007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia um número inteiro qualquer, e mostre na tela a sua tabuada.
+
+num = int(input('Digite um número que deseja saber sua tabuada: '))
+print(f'{num} X 1 = {num * 1}')
+print(f'{num} X 2 = {num * 2}')
