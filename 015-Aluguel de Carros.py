70f7f9c44fd49c62e541f025e29cd56d53312297
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-02-26 17:21:55 -0300

    Creaty  desafio 014 e 015

---
+++ 015-Aluguel de Carros.py
@@ -0,0 +1,5 @@
+# Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias locados.
+# Calcule o preço a pagar, sabendo que o carro custa R$60,00 por dia e R$0,15 por km rodados.
+
+carro_km_rodados = float(input('Digite quantos quilômetros o veículo rodou: '))
+carro_dias_locados = float(input('Digite a quantidade de dias locados: '))
